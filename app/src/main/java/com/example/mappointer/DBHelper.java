3d85d5b4048e1a44dee0f91cce2c878a8c4cafcd
Maxim Shabanov <99maxwolf99@gmail.com>
2024-06-04 21:26:43 +0300

    создал проект, добавил DBHelper для работы с SQLite

---
+++ app/src/main/java/com/example/mappointer/DBHelper.java
@@ -0,0 +1,108 @@
+package com.example.mappointer;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.mappointer.models.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "map_pointer.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "geo_points";
+    private static final String COLUMN_GEO_POINT_LATITUDE = "latitude";
+    private static final String COLUMN_GEO_POINT_LONGITUDE = "longitude";
+    private static final String COLUMN_DESCRIPTION = "description";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + "("
+                + "id UUID PRIMARY KEY, "
+                + COLUMN_GEO_POINT_LATITUDE + " TEXT , "
+                + COLUMN_GEO_POINT_LONGITUDE + " REAL"
+                + ")";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void insertOrUpdateExchangeRate(String latitude, String longitude, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_GEO_POINT_LATITUDE, latitude);
+        values.put(COLUMN_GEO_POINT_LONGITUDE, longitude);
+        values.put(COLUMN_DESCRIPTION, description);
+
+        long id = db.insertWithOnConflict(
+                TABLE_NAME,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE
+        );
+
+        db.close();
+    }
+
+    public Cursor getExchangeRate(String latitude, String longitude) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(
+                TABLE_NAME,
+                null,
+                COLUMN_GEO_POINT_LATITUDE + " = ? and "
+                        + COLUMN_GEO_POINT_LONGITUDE + " = ?",
+                new String[]{latitude, longitude},
+                null,
+                null,
+                null
+        );
+
+        return cursor;
+    }
+
+    public List<Point> getAllPoints() {
+        List<Point> points = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(
+                true,
+                TABLE_NAME,
+                new String[]{COLUMN_GEO_POINT_LATITUDE, COLUMN_GEO_POINT_LONGITUDE},
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                @SuppressLint("Range")String latitude = cursor.getString(cursor.getColumnIndex(COLUMN_GEO_POINT_LATITUDE));
+                @SuppressLint("Range")String longitude = cursor.getString(cursor.getColumnIndex(COLUMN_GEO_POINT_LONGITUDE));
+                Point point = new Point(latitude, longitude);
+                points.add(point);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return points;
+    }
+}
---
+++ app/src/main/java/com/example/mappointer/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mappointer;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private static final String API_KEY = "5770a16c-9f56-4da5-813c-95a4ad1c70ad";
+    private static final String API_KEY_LOCATOR = "282c1ae4-1d2b-41fe-a2a2-a51324fb8389";
+    private DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        dbHelper = new DBHelper(this);
+
+        button.setOnClickListener((v)->{
+
+        });
+
+    }
+
+}
